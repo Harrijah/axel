389ef0a1d6a41a0ba0256b3e17fd10383c1f819e
Hari <113083576+Harrijah@users.noreply.github.com>
2023-02-23 22:23:09 +0300

    reglage page d accueil

--- app/Controllers/Accueil.php
+++ app/Controllers/Accueil.php
@@ -6,6 +6,7 @@
         public function accueil()
         {
             return view('template-parts/header')
+            . view('template-parts/banneer01')
             . view('template-parts/footer');
         }
     }
---
+++ app/Views/template-parts/banneer01.php
@@ -0,0 +1,43 @@
+    <section class="principal-ban">
+        
+            <div class="principal01"><h1>Entreprises, développez votre potentiel</h1></div>
+            <div class="principal02"><img src="<?php echo base_url('image/entreprise.fw.png'); ?>" class="main-image" alt="Développez votre potentiel"></div>
+
+            <div class="product-card01">
+                <img src="<?php echo base_url('image/devweb.fw.png'); ?>" alt="Développement web et applications" class="image-separator">
+                <h2>Développement web et applications</h2>
+                <p>Création, refonte et maintenance de sites web. Développement d’application web, application mobile et application desktop.</p>
+                <a href="#">Nous contacter</a>
+            </div>
+            <div class="product-card02">
+                <div class="spacer"></div>
+                <img src="<?php echo base_url('image/seo02.png'); ?>" alt="Marketing digital" class="image-separator">
+                <h2>Marketing digital</h2>
+                <p>SEO, SEA, Growth hacking, Community management. Nous développons votre visibilité en ligne.</p>
+                <a href="#">Nous contacter</a>
+            </div>
+            <div class="product-card03">
+                <img src="<?php echo base_url('image/pencil.png'); ?>" alt="Création graphique" class="image-separator">                        
+                <h2>Création graphique</h2>
+                <p>Création de logo, création de bannières web et bannières publicitaires, création de visuels pour vos brochures, création de plan 3D et architectural</p>
+                <a href="#">Nous contacter</a>
+            </div>
+            <div class="product-card04">
+                <img src="<?php echo base_url('image/presentoir.fw.png'); ?>" alt="Création de supports" class="image-separator">
+                <h2>Création de supports</h2>
+                <p>Confection de plaque publicitaire, confection de présentoirs, PLV, impression 3D</p>
+                <a href="#">Nous contacter</a>
+            </div>
+                
+            
+    </section>
+
+    <div class="container">
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis feugiat ex quis nibh ullamcorper, a cursus lectus bibendum. Donec elementum magna eget elit blandit porttitor. Nunc euismod turpis vitae nunc semper luctus. Fusce eu nunc ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis feugiat ex quis nibh ullamcorper, a cursus lectus bibendum. Donec elementum magna eget elit blandit porttitor. Nunc euismod turpis vitae nunc semper luctus. Fusce eu nunc ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis feugiat ex quis nibh ullamcorper, a cursus lectus bibendum. Donec elementum magna eget elit blandit porttitor. Nunc euismod turpis vitae nunc semper luctus. Fusce eu nunc ipsum. </p>
+        </div>
+        <div class="background"></div>
+    </div>
+
+
+    
