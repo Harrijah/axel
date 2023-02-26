3a3327fa7bb0107caecb9d9312dc5b75942c7452
Hari <113083576+Harrijah@users.noreply.github.com>
2023-02-26 17:44:57 +0300

    rajout premiers effets

--- app/Views/template-parts/banneer01.php
+++ app/Views/template-parts/banneer01.php
@@ -7,26 +7,28 @@
                 <img src="<?php echo base_url('image/devweb.fw.png'); ?>" alt="Développement web et applications" class="image-separator">
                 <h2>Développement web et applications</h2>
                 <p>Création, refonte et maintenance de sites web. Développement d’application web, application mobile et application desktop.</p>
-                <a href="#">Nous contacter</a>
+                <a href="#" class="bouton01">En savoir plus</a>
             </div>
             <div class="product-card02">
                 <div class="spacer"></div>
                 <img src="<?php echo base_url('image/men-gba721144d_1280.png'); ?>" alt="Marketing digital" class="md">
                 <h2>Marketing digital</h2>
                 <p>SEO, SEA, Growth hacking, Community management. Nous développons votre visibilité en ligne.</p>
-                <a href="#">Nous contacter</a>
+                <a href="#" class="bouton01">En savoir plus</a>
             </div>
             <div class="product-card03">
-                <img src="<?php echo base_url('image/pencil.png'); ?>" alt="Création graphique" class="image-separator">                        
-                <h2>Création graphique</h2>
-                <p>Création de logo, création de bannières web et bannières publicitaires, création de visuels pour vos brochures, création de plan 3D et architectural</p>
-                <a href="#">Nous contacter</a>
+                <img src="<?php echo base_url('image/pencil.png'); ?>" alt="Création graphique" class="image-separator">
+                <div class="backme">
+                    <h2>Création graphique</h2>
+                    <p>Création de logo, création de bannières web et bannières publicitaires, création de visuels pour vos brochures, création de plan 3D et architectural</p>
+                    <a href="#" class="bouton02">En savoir plus</a>
+                </div>                        
             </div>
             <div class="product-card04">
                 <img src="<?php echo base_url('image/presentoir.fw.png'); ?>" alt="Création de supports" class="image-separator">
                 <h2>Création de supports</h2>
                 <p>Confection de plaque publicitaire, confection de présentoirs, PLV, impression 3D</p>
-                <a href="#">Nous contacter</a>
+                <a href="#" class="bouton01">En savoir plus</a>
             </div>
             <div class="nonecol01"></div>
             <div class="nonecol02"></div>
--- app/Views/template-parts/banneer02.php
+++ app/Views/template-parts/banneer02.php
@@ -8,7 +8,7 @@
             <p>
             Vous savez que le web est un terrain de jeu incontournable pour les entreprises d'aujourd'hui, mais vous ne savez pas comment vous démarquer sur ce marché saturé ? Comment toucher de nouveaux clients et fidéliser ceux que vous avez déjà ?  En optimisant votre présence en ligne grâce à des sites web optimisés et une publicité en ligne efficace. Nos prestations de développement de sites web et d'applications, de webmarketing et de conception graphique peuvent aider votre entreprise à atteindre son plein potentiel et à vous démarquer de la concurrence. Avec notre expertise en SEO, SEA et growth hacking, nous sommes déterminés à aider votre entreprise à atteindre ses objectifs sur le web. Contactez-nous dès maintenant pour en savoir plus sur la façon dont nous pouvons vous aider à développer votre entreprise sur le web.
             </p>
-            <a href="#">Nous contacter</a>
+            <a href="#" class="bouton01">Nous contacter</a>
         </div>
     </div>
     <div class="extraband">
@@ -18,7 +18,7 @@
                 <p>
                     Donnez vie à votre entreprise avec une communication visuelle captivante qui transmet votre message avec clarté et créativité. De la création de sites web personnalisés, à la conception de logos et de supports marketing imprimés, en passant par des photos et vidéos professionnelles, notre agence peut vous aider à véhiculer une image de marque forte, professionnelle, cohérente et mémorable pour votre entreprise. Nous offrons également des services de référencement, de publicité en ligne et de gestion de communauté pour aider à promouvoir votre entreprise auprès du public cible. Laissez-nous vous aider à créer une communication visuelle exceptionnelle qui reflète l'essence même de votre entreprise et attire l'attention de votre public.
                 </p>
-                <a href="#">Nous contacter</a>
+                <a href="#" class="bouton02">Nous contacter</a>
             </div>
             <div class="imagb02"><img src="<?php echo base_url('image/magician.fw.png') ?>" alt="Une bonne image de marque" class="imagimb02"></div>
         </div>
@@ -30,7 +30,7 @@
             <p>
                 Votre entreprise a un potentiel et il est temps de le montrer au monde ! Avec une présence en ligne à la hauteur de votre entreprise, vous pouvez bousculer votre marché et toucher un public plus large. Ne laissez plus vos doutes vous retenir : il est temps de faire le pas et de faire la différence. Toutes les ressources s’offrent à vous pour réaliser vos ambitions et concrétiser votre vision. N'attendez plus pour être acteur de votre succès, avec la force de votre entreprise, vous pouvez conquérir de nouveaux territoires et réaliser des exploits incroyables !
             </p>
-            <a href="#">Nous contacter</a>
+            <a href="#" class="bouton01">Nous contacter</a>
         </div>
     </div>
 </section>
