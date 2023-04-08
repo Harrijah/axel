d6f234d4373f4a7f4c1a8ea15681e3a367cec7fa
Hari <113083576+Harrijah@users.noreply.github.com>
2023-04-08 21:55:05 +0300

    Update header.php

--- app/Views/template-parts/header.php
+++ app/Views/template-parts/header.php
@@ -11,6 +11,17 @@
     <link rel="stylesheet" href="<?php echo base_url('css/style.css'); ?>">
     <title>Bienvenue sur Axel</title>
 </head>
+    
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DLVBHJWVDN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DLVBHJWVDN');
+</script>
+
 <body>
     <nav class="navprinc">
         <div class="logo-image">
