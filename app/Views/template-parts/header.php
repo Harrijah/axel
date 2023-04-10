aadc59f5cabd9daae3573e697ad90eb6ff69e1e6
Hari <113083576+Harrijah@users.noreply.github.com>
2023-04-10 09:47:44 +0300

    developpement

--- app/Views/template-parts/header.php
+++ app/Views/template-parts/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo base_url('font-awesome/font-awesome4/css/font-awesome.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('font-awesome/font-awesome4/css/font-awesome.min.css'); ?>">
+    
     <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/bootstrap.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/bootstrap.min.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('css/style.css'); ?>">
--- app/Views/template-parts/services/banniere01.php
+++ app/Views/template-parts/services/banniere01.php
@@ -1,7 +1,7 @@
 <section>
     <div class="service-back">
         <div class="service-title">
-            <h1>Développement web</h1>
+            <h1>Développement web <i class="fa fa-laptop facode"></i></h1>
         </div>
         <div class="picto"></div>
     </div>
@@ -21,26 +21,39 @@
         
         <div class="serv-container">
             <div class="les-services">
-                <div class="vide01"></div>
-                <div class="vide02"></div>
-                <div class="vide03"></div>
-                <div class="vide04"></div>
-                <div class="sur-mesure">
-                    <h2 class="green-title02">Site sur-mesure</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt aut porro vitae a rem eaque sit quae consectetur. Et officia, dicta repellat aut quasi ad earum amet sunt sapiente quia.</p>
+
+                <div class="service-det01">
+                    <div class="sur-mesure">
+                        <h2 class="green-title02">Site sur-mesure</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt aut porro vitae a rem eaque sit quae consectetur. Et officia, dicta repellat aut quasi ad earum amet sunt sapiente quia.</p>
+                    </div>
+                    <div class="vide01"></div>
                 </div>
-                <div class="e-commerce">
-                    <h2 class="green-title02">Site e-commerce</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam blanditiis repellat iste error voluptatum rerum iure inventore consectetur, nesciunt quidem saepe voluptatem voluptas! Ipsam nihil tempora libero sed. Perspiciatis, natus.</p>
+
+                <div class="service-det02">
+                    <div class="vide02"></div>
+                    <div class="e-commerce">
+                        <h2 class="green-title02">Site e-commerce</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam blanditiis repellat iste error voluptatum rerum iure inventore consectetur, nesciunt quidem saepe voluptatem voluptas! Ipsam nihil tempora libero sed. Perspiciatis, natus.</p>
+                    </div>
                 </div>
-                <div class="site-vitrine">
-                    <h2 class="green-title02">Site vitrine</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore impedit facere veniam corrupti asperiores tenetur quasi sint, harum rerum, culpa quos deleniti a nostrum totam sit ipsum, molestiae nulla necessitatibus!</p>
+
+                <div class="service-det03">
+                    <div class="site-vitrine">
+                        <h2 class="green-title02">Site vitrine</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore impedit facere veniam corrupti asperiores tenetur quasi sint, harum rerum, culpa quos deleniti a nostrum totam sit ipsum, molestiae nulla necessitatibus!</p>
+                    </div>
+                    <div class="vide03"></div>
                 </div>
-                <div class="applications">
-                    <h2 class="green-title02">Applications web et mobile</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur nisi doloribus laudantium pariatur, nostrum sequi et similique recusandae delectus voluptates consectetur, natus explicabo! Consequatur consequuntur labore, totam repellendus architecto deserunt!</p>
+
+                <div class="service-det04">
+                    <div class="vide04"></div>
+                    <div class="applications">
+                        <h2 class="green-title02">Applications web et mobile</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur nisi doloribus laudantium pariatur, nostrum sequi et similique recusandae delectus voluptates consectetur, natus explicabo! Consequatur consequuntur labore, totam repellendus architecto deserunt!</p>
+                    </div>
                 </div>
+
             </div>
         </div>
         
