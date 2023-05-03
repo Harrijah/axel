6bc0a3c9e990b0705ae508db1b78890ecaa89a73
Hari <113083576+Harrijah@users.noreply.github.com>
2023-05-03 10:45:44 +0300

    sass départ 0

--- app/Views/template-parts/accueil/banneer01.php
+++ app/Views/template-parts/accueil/banneer01.php
@@ -1,62 +1,69 @@
     <section class="principal-ban">
-            <div class="principal01">
-                <h1>Entreprise, développez votre potentiel !</h1>
-            </div>
-            <div class="principal02">
-                <div class="interm">
-                    <img src="<?php echo base_url('image/resized/entreprise.fw.png'); ?>" class="main-image" alt="Développez votre potentiel">
-                    </div>
-                </div>
+        <div class="principal01">
+            <h1>Entreprise, développez votre potentiel !</h1>
+        </div>
 
-            <div class="product-card01">
-                <img src="<?php echo base_url('image/resized/devweb.fw.png'); ?>" alt="Développement web et applications" class="image-separator">
-                <div class="separator"></div>
-                <div class="backme02">
-                    <h2>Développement web et applications</h2>
-                    <p>Création, refonte et maintenance de sites web. Développement d’application web, application mobile et application desktop.</p>
-                    <a href="#" class="bouton01">En savoir plus</a>
-                </div>
+        <div class="principal02">
+            <div class="interm">
+                <img src="<?php echo base_url('image/resized/entreprise.fw.png'); ?>" class="main-image" alt="Développez votre potentiel">
             </div>
-            <div class="product-card02">
+        </div>
+
+        <div class="product-card01">
+            <img src="<?php echo base_url('image/resized/devweb.fw.png'); ?>" alt="Développement web et applications" class="image-separator">
+            <div class="separator"></div>
+            <div class="backme02">
+                <h2>Développement web et applications</h2>
+                <p>Création, refonte et maintenance de sites web. Développement d’application web, application mobile et application desktop.</p>
+                <a href="#" class="bouton01">En savoir plus</a>
+            </div>
+        </div>
+        <div class="product-card02">
+            <div>
                 <div class="spacer"></div>
                 <img src="<?php echo base_url('image/resized/men-gba721144d_1280.png'); ?>" alt="Marketing digital" class="md">
-            <div class="separator"></div>
+                <div class="separator"></div>
                 <div class="backme02">
                     <h2>Marketing digital</h2>
                     <p>SEO, SEA, Growth hacking, Community management. Nous développons votre visibilité en ligne.</p>
                     <a href="#" class="bouton01">En savoir plus</a>
                 </div>
             </div>
-            <div class="product-card03">
-                <img src="<?php echo base_url('image/resized/pencil.png'); ?>" alt="Création graphique" class="image-separator">
+        </div>
+
+        <div class="product-card03">
+            <img src="<?php echo base_url('image/resized/pencil.png'); ?>" alt="Création graphique" class="image-separator">
             <!-- <div class="separator"></div> -->
-                <div class="backme">
-                    <h2>Création graphique</h2>
-                    <p>Création de logo, création de bannières web et bannières publicitaires, création de visuels pour vos brochures, création de plan 3D et architectural</p>
-                    <a href="#" class="bouton02">En savoir plus</a>
-                </div>                        
-            </div>
-            <div class="product-card04">
-                <img src="<?php echo base_url('image/resized/presentoir.fw.png'); ?>" alt="Création de supports" class="image-separator">
+            <div class="backme">
+                <h2>Création graphique</h2>
+                <p>Création de logo, création de bannières web et bannières publicitaires, création de visuels pour vos brochures, création de plan 3D et architectural</p>
+                <a href="#" class="bouton02">En savoir plus</a>
+            </div>                        
+        </div>
+
+        <div class="product-card04">
+            <img src="<?php echo base_url('image/resized/presentoir.fw.png'); ?>" alt="Création de supports" class="image-separator">
             <div class="separator"></div>
-                <div class="backme02">
-                    <h2>Création de supports</h2>
-                    <p>Confection de plaque publicitaire, confection de présentoirs, PLV, impression 3D</p>
-                    <a href="#" class="bouton01">En savoir plus</a>
-                </div>
+            <div class="backme02">
+                <h2>Création de supports</h2>
+                <p>Confection de plaque publicitaire, confection de présentoirs, PLV, impression 3D</p>
+                <a href="#" class="bouton01">En savoir plus</a>
             </div>
-            <div class="nonecol01"></div>
-            <div class="nonecol02"></div>
-            <div class="news01">
-                <div>
-                    <h3>Vous avez un besoin ?</h3>
-                    <form action="" method="post">
-                        <textarea name="" id="" cols="50" rows="5" placeholder="Décrivez-le nous en quelques mots !"></textarea>
-                        <input type="email" name="email" id="email" placeholder="Votre e-mail">
-                        <button type="submit" id="mydemand">Soumettre</button>
-                    </form>
-                </div>
+        </div>
+        <!-- <div class="nonecol01"></div>
+        <div class="nonecol02"></div> -->
+
+        <div class="news01">
+            <div>
+                <h3>Vous avez un besoin ?</h3>
+                <form action="" method="post">
+                    <textarea name="" id="" cols="50" rows="5" placeholder="Décrivez-le nous en quelques mots !"></textarea>
+                    <input type="email" name="email" id="email" placeholder="Votre e-mail">
+                    <button type="submit" id="mydemand">Soumettre</button>
+                </form>
             </div>
+        </div>
+       
                 
             
     </section>
--- app/Views/template-parts/header.php
+++ app/Views/template-parts/header.php
@@ -4,15 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo base_url('font-awesome/font-awesome4/css/font-awesome.css'); ?>">
-    <link rel="stylesheet" href="<?php echo base_url('font-awesome/font-awesome4/css/font-awesome.min.css'); ?>">
-    <link rel="stylesheet" href="<?php echo base_url('font-awesome/font-awesome5/css/fontawesome.css'); ?>">
-    <link rel="stylesheet" href="<?php echo base_url('font-awesome/font-awesome5/css/fontawesome.min.css'); ?>">
+    <link rel="stylesheet" href="<?php //echo base_url('font-awesome/font-awesome4/css/font-awesome.css'); ?>">
+    <link rel="stylesheet" href="<?php //echo base_url('font-awesome/font-awesome4/css/font-awesome.min.css'); ?>">
+    <link rel="stylesheet" href="<?php //echo base_url('font-awesome/font-awesome5/css/fontawesome.css'); ?>">
+    <link rel="stylesheet" href="<?php //echo base_url('font-awesome/font-awesome5/css/fontawesome.min.css'); ?>">
     
     <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/bootstrap.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('bootstrap/css/bootstrap.min.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('styles/index.css'); ?>">
-    <!-- <link rel="stylesheet" href="<?php echo base_url('styles/style02.css'); ?>"> -->
     <title>Bienvenue sur Axel</title>
 </head>
     
@@ -38,7 +37,7 @@
                 <li class="menu"><a href="<?php echo base_url('/') ?>" class="menubutton">Accueil</a></li>
                 <li class="menu"><a href="<?php echo base_url('/') ?>" class="menubutton">A propos de nous</a></li>
                 <li class="menu dropmoth">
-                    <span class="menu-services">Nos services</span>
+                    <div class="menu-services">Nos services</div>
                     <ul class="menu-drop">
                         <li class="deroul-menu"><a href="<?php echo base_url('development') ?>" class="menubutton">Développement web & app</a></li>
                         <li class="deroul-menu"><a href="<?php echo base_url('marketing') ?>" class="menubutton">Marketing digital</a></li>
